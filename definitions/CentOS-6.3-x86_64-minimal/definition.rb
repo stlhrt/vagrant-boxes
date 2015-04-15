90ee645fa538d62d0f8b196d97dfbb3a299e96b2
Lukasz Wozniak <stlhrt@gmail.com>
2015-04-16 01:10:46 +0200

    Correct build with oracle install and cleanup

--- definitions/CentOS-6.3-x86_64-minimal/definition.rb
+++ definitions/CentOS-6.3-x86_64-minimal/definition.rb
@@ -1,8 +1,9 @@
 Veewee::Definition.declare({
 	:hooks => {
 		:before_postinstall => Proc.new { 
-			definition.box.copy('oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip', '/tmp/oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip')
-			definition.box.copy('oracle/xe.rsp', '/tmp/oracle/xe.rsp')
+			definition.box.exec('mkdir /tmp/oracle')
+			definition.box.scp('oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip', '/tmp/oracle/oracle-xe-11.2.0-1.0.x86_64.rpm.zip')
+			definition.box.scp('oracle/xe.rsp', '/tmp/oracle/xe.rsp')
 		}
 	},
   :cpu_count => '1',
--- definitions/CentOS-6.3-x86_64-minimal/oracle.sh
+++ definitions/CentOS-6.3-x86_64-minimal/oracle.sh
@@ -9,6 +9,4 @@ rpm -ivh oracle-xe-11.2.0-1.0.x86_64.rpm
 /etc/init.d/oracle-xe configure responseFile=/tmp/oracle/xe.rsp
 
 # Cleanup
-rm -rf /home/veewee/oracle
-rm -rf /home/veewee/oracle-xe-11.2.0-1.0.x86_64.rpm.zip
-
+rm -rf /tmp/oracle
