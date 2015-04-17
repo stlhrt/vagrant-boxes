ae2ba6baacc32c38f87ec1834b4dd59cd7fb416b
Lukasz Wozniak <stlhrt@gmail.com>
2015-04-17 18:48:45 +0200

    Fixed firewall and ipv6 disabling

--- definitions/CentOS-6.3-x86_64-minimal/oracle.sh
+++ definitions/CentOS-6.3-x86_64-minimal/oracle.sh
@@ -1,6 +1,9 @@
 # Installing the oracle XE DB
 
+echo "Disabling IPv6 ####################################################"
 # Disable ipv6
+sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl -w net.ipv6.conf.default.disable_ipv6=1
 echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
 echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
 
